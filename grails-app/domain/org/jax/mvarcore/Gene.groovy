1f12ad4b8899bbcfa9888a54d1b216cbc1ff7bb8
Baha El Kassaby <baha.elkassaby@jax.org>
2019-11-05 13:42:09 -0500

    [MVAR-73] change gene model to be like mmrdb

--- grails-app/domain/org/jax/mvarcore/Gene.groovy
+++ grails-app/domain/org/jax/mvarcore/Gene.groovy
@@ -2,11 +2,28 @@ package org.jax.mvarcore
 
 class Gene {
 
+    String mgiId
+    String symbol
     String name
-    String chromosome
+    String description
+    String chr
+    String type
+    String entrezGeneId
+    String ensemblGeneId
+
+    static constraints = {
+
+        entrezGeneId nullable: true
+        ensemblGeneId nullable:  true
+        description nullable: true, sqlType: 'text'
+        type nullable: true
+    }
+
+    static mapping = {
+        symbol index:'symbol_idx'
+
+    }
 
     static hasMany = [identifiers: Identifier, variants: Variant]
 
-    static constraints = {
-    }
 }
