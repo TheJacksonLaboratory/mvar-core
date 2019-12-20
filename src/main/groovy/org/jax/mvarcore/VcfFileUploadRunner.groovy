8d2d37d3bbae01e1272d07ac4d7b1c88e3cf52e7
Baha El Kassaby <baha.elkassaby@jax.org>
2019-12-20 11:20:52 -0500

    [MVAR-77] Create a Runner class that triggers after the app startup
    *If an argument is given at application startup (folder path expected),
    the folder is parsed for gz and vcf files and the variations found are
    inserted in the DB.

---
+++ src/main/groovy/org/jax/mvarcore/VcfFileUploadRunner.groovy
@@ -0,0 +1,40 @@
+package org.jax.mvarcore
+
+import groovy.io.FileType
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+
+/**
+ * Class that will run after the application has been started.
+ * Takes the first program argument provided by the application.
+ * A folder absolute path is expected. Then the folder is recursively searched
+ * and a list of .gz and.vcf files is populated and parsed to be inserted in the DB.
+ */
+class VcfFileUploadRunner implements CommandLineRunner {
+    @Autowired
+    VcfFileUploadService vcfFileUploadService
+
+    @Override
+    void run(String...args) throws Exception {
+        String folderPath
+        if (args != null) {
+            folderPath = args[0]
+        } else {
+            return
+        }
+        List<File> list = []
+        File dir = new File(folderPath)
+        dir.eachFileRecurse (FileType.FILES) { file ->
+            list << file
+        }
+
+        list.each { it ->
+            String path = it.path
+            File file = new File(path)
+            if (file.isFile() && (path.endsWith('.gz')||(path.endsWith('.vcf'))))
+                vcfFileUploadService.loadVCF(file)
+        }
+    }
+
+}
+
