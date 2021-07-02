dfbff1bf71f6b4690a0b986d029154295abc897d
Baha El Kassaby <Baha.ElKassaby@jax.org>
2021-07-02 14:37:52 -0400

    MVAR-134 add new column to variant_strain
    *add new column to keep track of the relationship imputedness

--- grails-app/domain/org/jax/mvarcore/VariantStrain.groovy
+++ grails-app/domain/org/jax/mvarcore/VariantStrain.groovy
@@ -3,14 +3,18 @@ package org.jax.mvarcore
 class VariantStrain {
 
     String genotype
+    /* 0=non-imputed, 1=imputed-snpgrid, 2=imputed-mgi, 3=, 4=  */
+    Integer imputed
 
     static belongsTo = [strain: Strain, variant: Variant]
     static constraints = {
         genotype nullable: false
+        imputed nullable: true
     }
 
     static mapping = {
         genotype sqlType: "char(5)"
+        imputed sqlType: "tinyint"
         version false
     }
 }
