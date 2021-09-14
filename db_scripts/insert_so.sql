d7659b8f40cda350675cee6fca96096d326c8017
Baha El Kassaby <Baha.ElKassaby@jax.org>
2021-09-14 17:30:49 -0400

    Add SQL insertion scripts for strain and so

---
+++ db_scripts/insert_so.sql
@@ -0,0 +1,18 @@
+### anticipation is that this is run by a user with the required permissions
+### root would have this
+
+## sort table columns
+ALTER TABLE strain MODIFY label varchar(255) AFTER so_id;
+ALTER TABLE strain MODIFY sub_class_of varchar(255) AFTER label;
+ALTER TABLE strain MODIFY definition text AFTER suc_class_of;
+
+# this Sequence Ontology file has been generated with robot, an ontology tool
+# and was converted to TSV from an OWL file (found on https://github.com/The-Sequence-Ontology/SO-Ontologies/blob/master/Ontology_Files/so.owl)
+LOAD DATA LOCAL INFILE '/local/svc-mvr/mvar-db/mysql/so.csv'
+INTO TABLE sequence_ontology
+FIELDS TERMINATED BY '\t'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES (so_id, label, sub_class_of, definition);
+
+commit
